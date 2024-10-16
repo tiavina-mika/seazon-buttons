aa961d9f75d800479f0d51365c558b59da1e3a4c
tiavina-mika <tiavinamika@gmail.com>
2024-10-16 13:04:48 +0300

    Create Preferences.jsx

---
+++ components/components/icons/Preferences.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Icon from "./icon/Icon";
+import PropTypes from "prop-types";
+
+const Preferences = ({ onClick, size, isNotification = false }) => {
+  return (
+    <Icon
+      name={`preferences${isNotification ? "-notification" : ""}`}
+      ariaLabel="Filtrer"
+      onClick={onClick}
+      size={size}
+    />
+  );
+};
+
+Preferences.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  size: PropTypes.oneOf(["xs", "sm", "md"]),
+  isNotification: PropTypes.bool,
+};
+
+export default Preferences;
