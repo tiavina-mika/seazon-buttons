713fe7b9ec481edb66c3d92942ea0482646e3cd8
tiavina-mika <tiavinamika@gmail.com>
2024-10-18 15:13:22 +0300

    Create ApplastoreButton.jsx

---
+++ container/components/buttons/store/ApplastoreButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ApplastoreButton = () => {
+  return (
+    <a href="/">
+      <img src="/icons/applestore-download.svg" alt="" />
+    </a>
+  );
+};
+
+export default ApplastoreButton;
