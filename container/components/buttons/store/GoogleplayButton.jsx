32fbec1bcef7cee4a7d69459709dad977ddb981a
tiavina-mika <tiavinamika@gmail.com>
2024-10-18 15:13:26 +0300

    Create GoogleplayButton.jsx

---
+++ container/components/buttons/store/GoogleplayButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GoogleplayButton = () => {
+  return (
+    <a href="/">
+      <img src="/icons/googleplay-download.svg" alt="" />
+    </a>
+  );
+};
+
+export default GoogleplayButton;
