040cdbc9b27bdd6297ad9ea96773b0e4ccdfc25a
tiavina-mika <tiavinamika@gmail.com>
2024-11-17 09:18:50 +0300

    Create ButtonLine.stories.jsx

---
+++ stories/components/buttonsV2/ButtonLine.stories.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import ButtonComponent from '../../../components/buttons/button/ButtonLine';
+
+
+const meta = {
+  title: 'New design system/components/buttons/ButtonLine',
+  component: ButtonComponent,
+  parameters: {
+    layout: 'centered'
+  }
+};
+
+export default meta;
+
+export const ButtonLine = {
+  args: {
+    variant: 'button'
+  }
+};
