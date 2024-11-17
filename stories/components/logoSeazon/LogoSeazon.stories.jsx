ec2c5bf623a6199140355a0ba1118d86b6a32b39
tiavina-mika <tiavinamika@gmail.com>
2024-11-17 09:27:21 +0300

    Create LogoSeazon.stories.jsx

---
+++ stories/components/logoSeazon/LogoSeazon.stories.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import LogoSeazon from "../../../components/logoSeazon/LogoSeazon";
+
+const meta = {
+  title: 'New design system/components/LogoSeazon',
+  component: LogoSeazon,
+  parameters: {
+    layout: 'centered',
+  },
+};
+export default meta;
+
+export const LogoSeazonExample = {
+  args: {
+    configuration: '32',
+  },
+};
+
+LogoSeazonExample.storyName = 'LogoSeazon';
