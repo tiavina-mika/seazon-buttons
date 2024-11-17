8428852290ad8de8d9359430edb2625afcea6c1b
tiavina-mika <tiavinamika@gmail.com>
2024-11-17 09:23:57 +0300

    Create EcoScoreBlack.stories.jsx

---
+++ stories/components/icons/EcoScoreBlack.stories.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import EcoScoreBlackIcon from "../../../components/icons/EcoScoreBlack";
+
+
+const meta = {
+  title: 'New design system/components/Icons/EcoScoreBlack',
+  component: EcoScoreBlackIcon,
+  parameters: {
+    layout: 'centered',
+  },
+};
+export default meta;
+
+export const EcoScoreBlack = {
+  args: {
+    grade: 'A',
+    size: 'sm',
+    color: '#003840',
+    disabled: false
+  }
+};
