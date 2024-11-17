b17a3bb0c43aab25c742c9c63a66d0461e256e16
tiavina-mika <tiavinamika@gmail.com>
2024-11-17 09:27:18 +0300

    Create LogoResponsiveSeazon.stories.jsx

---
+++ stories/components/logoSeazon/LogoResponsiveSeazon.stories.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import LogoResponsiveSeazon from "../../../components/logoSeazon/LogoResponsiveSeazon";
+
+const meta = {
+  title: 'New design system/components/LogoSeazon',
+  component: LogoResponsiveSeazon,
+  parameters: {
+    layout: 'centered',
+  },
+};
+export default meta;
+
+export const LogoResponsiveSeazonExample = {
+  args: {
+    configuration: 'default',
+  },
+};
+
+LogoResponsiveSeazonExample.storyName = 'LogoResponsiveSeazon';
