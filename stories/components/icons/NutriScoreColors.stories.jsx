df09becad60ebb236de0830b90c1f07a4f6b5298
tiavina-mika <tiavinamika@gmail.com>
2024-11-17 09:25:21 +0300

    Create NutriScoreColors.stories.jsx

---
+++ stories/components/icons/NutriScoreColors.stories.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import NutriScoreColorsIcon from "../../../components/icons/NutriScoreColors";
+
+
+const meta = {
+  title: 'New design system/components/Icons/NutriScoreColors',
+  component: NutriScoreColorsIcon,
+  parameters: {
+    layout: 'centered',
+  },
+};
+export default meta;
+
+export const NutriScoreColors = {
+  args: {
+    grade: 'A',
+    disabled: false
+  }
+};
