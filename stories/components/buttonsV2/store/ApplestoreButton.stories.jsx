13bfc97b7f41ed623e94f30f2eadf1ce2c2f40a3
tiavina-mika <tiavinamika@gmail.com>
2024-11-17 09:19:25 +0300

    Create ApplestoreButton.stories.jsx

---
+++ stories/components/buttonsV2/store/ApplestoreButton.stories.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ApplestoreButton from '../../../../components/buttons/store/AppleStoreButton';
+
+const meta = {
+  title: 'New design system/components/buttons/store/AppleStore',
+  component: ApplestoreButton,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+
+export const AppleStore = {
+  args: {
+  },
+};
