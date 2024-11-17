fb77ab02d1361598202663d4e680edb5369f7f78
tiavina-mika <tiavinamika@gmail.com>
2024-11-17 09:19:29 +0300

    Create GoogleplayButton.stories.jsx

---
+++ stories/components/buttonsV2/store/GoogleplayButton.stories.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import GoogleplayButton from '../../../../components/buttons/store/GooglePlayButton';
+
+const meta = {
+  title: 'New design system/components/buttons/store/GooglePlay',
+  component: GoogleplayButton,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+
+export const GooglePlay = {
+  args: {
+  },
+};
