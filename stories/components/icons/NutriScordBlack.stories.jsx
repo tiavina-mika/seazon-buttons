0a37682336fc5430623cd03a7c3e407f37dabf72
tiavina-mika <tiavinamika@gmail.com>
2024-11-17 09:25:18 +0300

    Create NutriScordBlack.stories.jsx

---
+++ stories/components/icons/NutriScordBlack.stories.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import NutriScoreBlackIcon from "../../../components/icons/NutriScoreBlack";
+
+
+const meta = {
+  title: 'New design system/components/Icons/NutriScoreBlack',
+  component: NutriScoreBlackIcon,
+  parameters: {
+    layout: 'centered',
+  },
+};
+export default meta;
+
+export const NutriScoreBlack = {
+  args: {
+    grade: 'A',
+    size: 'sm',
+    color: '#003840',
+    disabled: false
+  }
+};
