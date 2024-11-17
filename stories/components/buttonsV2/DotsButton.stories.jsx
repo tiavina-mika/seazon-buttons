6bfd9e221f52a8d0c2b4742b40dd9e5b95f1ffcd
tiavina-mika <tiavinamika@gmail.com>
2024-11-17 09:19:08 +0300

    Create DotsButton.stories.jsx

---
+++ stories/components/buttonsV2/DotsButton.stories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import DotsButton from '../../../components/buttons/dots/DotsButton';
+
+const meta = {
+  title: 'New design system/components/buttons/Dots',
+  component: DotsButton,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+
+export const Dots = {
+  args: {
+    currentIndex: 1,
+  },
+};
+
