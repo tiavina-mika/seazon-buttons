871064c6360576dafddabe2abefb53fa67d2c626
tiavina-mika <tiavinamika@gmail.com>
2024-11-17 09:19:22 +0300

    Create SliderButton.stories.jsx

---
+++ stories/components/buttonsV2/SliderButton.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SliderButton from '../../../components/buttons/sliderButton/SliderButton';
+
+const meta = {
+  title: 'New design system/components/buttons/Slider',
+  component: SliderButton,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+
+export const Slider = {
+  args: {
+  },
+};
+
