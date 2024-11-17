acc47f6252b66576b99323af7d2c895a5e27d519
tiavina-mika <tiavinamika@gmail.com>
2024-11-17 09:19:32 +0300

    Create Switch.stories.jsx

---
+++ stories/components/buttonsV2/Switch.stories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import SwitchComponent from '../../../components/buttons/switch/Switch';
+
+const meta = {
+  title: 'New design system/components/buttons/Switch',
+  component: SwitchComponent,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+
+export const Switch = {
+  args: {
+    length: 3
+  },
+};
+
